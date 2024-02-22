6c018724a09d47ddf99646520d140ab9842c33bb
Lucilene Martins <lucy-16-rj@hotmail.com>
2024-02-21 21:19:57 -0300

    adding task 1 in c#

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+public class Logger
+{
+    public static void LogMessage(string filename, string message, string level)
+    {
+        string timestamp = DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss]");
+        string logEntry = $"{timestamp} [{level}] {message}\n";
+        File.AppendAllText(filename, logEntry);
+    }
+}
+
+// NUnit Test Cases
+
+[TestFixture]
+public class LoggerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        // Clean up existing log file
+        File.WriteAllText("test.log", string.Empty);
+    }
+
+    [Test]
+    public void LogMessage_WritesLogEntryToFile()
+    {
+        // Arrange
+        string filename = "test.log";
+        string message = "User logged in";
+        string level = "INFO";
+
+        // Act
+        Logger.LogMessage(filename, message, level);
+
+        // Assert
+        string[] logContent = File.ReadAllLines(filename);
+        Assert.Equals(1, logContent.Length);
+        Assert.That(logContent[0].StartsWith("["));
+        Assert.That(logContent[0], Does.Contain("[INFO] User logged in"));
+    }
+
+    [Test]
+    public void LogMessage_WritesMultipleLogEntriesToFile()
+    {
+        // Arrange
+        string filename = "test.log";
+        
+        // Act
+        Logger.LogMessage(filename, "User logged in", "INFO");
+        Logger.LogMessage(filename, "Failed login attempt", "WARNING");
+
+        // Assert
+        string[] logContent = File.ReadAllLines(filename);
+        Assert.Equals(2, logContent.Length);
+        Assert.That(logContent[0].Contains("[INFO] User logged in"));
+        Assert.That(logContent[1].Contains("[WARNING] Failed login attempt"));
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# dev_pro_dev_c#
+# :woman_technologist: dev_pro_dev_c# Task
+
+Project Dev.Pro task in C#
+
+Tools and Libs:
+
+- C#
+- NUnit
+
+
+## 🎯 Prerequisites
+
+<b>Install   </b></br> 
+ </br>
+
+<b>Install  </b></br>
+ </br>
+
+<b>Clone project </b></br>
+
+<b>Install dependencies  </b> </br>
+ $ npm install</br>
+ 
+ Done ✔️
+
+
+## 💡 Use
+
+<b>Run tests </b></br>
+$ npm test </br>
+
+
+
+
